b8b466f8a11be251870a743d3b22a76e1b744611
Lucus Lally <lucuslally1@gmail.com>
2018-08-14 21:07:19 -0700

    Adding new calculatePrice and orderSummary methods

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,5 +1,5 @@
 /**
- * IMPORTANT: Make sure you are using the correct package name. 
+ * IMPORTANT: Make sure you are using the correct package name.
  * This example uses the package name:
  * package com.example.android.justjava
  * If you get an error when copying this code into Android studio, update it to match teh package name found
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
 import java.text.NumberFormat;
 
 import static com.example.android.justjava.R.layout.activity_main;
@@ -21,7 +22,7 @@ import static com.example.android.justjava.R.layout.activity_main;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-int quantity = 0;
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,49 +33,64 @@ int quantity = 0;
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder (View view) {
-        int price = quantity * 5;
-        String priceMessage = "Total: $" + price + "\nThank You!";
-        displayMessage(priceMessage);
+    public void submitOrder(View view) {
+        int price = calculatePrice(quantity, 5);
+        displayMessage(createOrderSummary(price));
+    }
+
+    /**
+     * Create summary of the order
+     *
+     * @param price value used previously for calculatePrice method
+     * @return string summary
+     */
+
+    private String createOrderSummary(int price) {
+        String priceMessage = "Name: Kaptain Kunal";
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: $" + price;
+        priceMessage += "\nThank You!";
+        return priceMessage;
+    }
+
+    /**
+     * This method calculates the price based on the current quantity
+     *
+     * @param quantity is number of coffees ordered
+     */
+    private int calculatePrice(int quantity, int pricePerCup) {
+        return quantity * pricePerCup;
     }
 
     /**
      * This method is called when the plus button is clicked
      **/
-    public void incrementOrder (View view){
+    public void incrementOrder(View view) {
         quantity++;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      * This method is called when the minus button is clicked
      **/
-    public void decrementOrder (View view){
+    public void decrementOrder(View view) {
         quantity--;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int numberOfCoffees) {
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-    }
-
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        quantityTextView.setText("" + numberOfCoffees);
     }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 }
